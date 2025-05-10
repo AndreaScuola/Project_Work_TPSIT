0db9469c6525f06ef7ae35776b0ff50ed060840b
AndreaScuola <162697678+AndreaScuola@users.noreply.github.com>
2025-05-10 11:23:51 +0200

    Commit some code

---
+++ include/ImpiantoAlpino.h
@@ -0,0 +1,8 @@
+//
+// Created by Standard on 10/05/2025.
+//
+
+#ifndef IMPIANTOALPINO_H
+#define IMPIANTOALPINO_H
+
+#endif //IMPIANTOALPINO_H
---
+++ src/Impianto.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Standard on 10/05/2025.
+//
