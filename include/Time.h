e8abf5c5498706fa2d933ef10f4cddc5cdb37c45
Aleksa Ristic <162697425+Aleksa017@users.noreply.github.com>
2025-05-11 21:35:16 +0200

    ho fatto la classe time

--- include/Time.h
+++ include/Time.h
@@ -1,11 +1,24 @@
-//Qui bisogna dichiarare costanti e metodi
-
 #ifndef TIME_H
 #define TIME_H
+
+#include <string>
+
 class Time {
 private:
+    int hour;
+    int minute;
 
 public:
+    static const int MIN_HOUR   = 0;
+    static const int MAX_HOUR   = 23;
+    static const int MIN_MINUTE = 0;
+    static const int MAX_MINUTE = 59;
 
+    Time(int h = 0, int m = 0);
+
+    void Setter(int h, int m);
+    Time& operator++();
+    std::string GetTime() const;
 };
-#endif //TIME_H
+
+#endif // TIME_H
--- src/Time.cpp
+++ src/Time.cpp
@@ -1,1 +1,38 @@
-#include "../include/Time.h"    //Collega il .cpp al .h
+#include "time.h"
+#include <sstream>
+#include <iomanip>
+
+Time::Time(int h, int m) {
+    Setter(h, m);
+}
+
+void Time::Setter(int h, int m) {
+    if (m > MAX_MINUTE) {
+        h += m / 60;
+        m = m % 60;
+    }
+    if (h > MAX_HOUR) {
+        h = h % 24;
+    }
+    hour = h;
+    minute = m;
+}
+
+Time& Time::operator++() {
+    ++minute;
+    if (minute > MAX_MINUTE) {
+        minute = 0;
+        ++hour;
+        if (hour > MAX_HOUR)
+            hour = 0;
+    }
+    return *this;
+}
+
+std::string Time::GetTime() const {
+    std::ostringstream oss;
+    oss << std::setw(2) << std::setfill('0') << hour
+        << ":"
+        << std::setw(2) << std::setfill('0') << minute;
+    return oss.str();
+}
